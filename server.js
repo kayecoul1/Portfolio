08c8da738233ea9b19af005c4f5bb04e5eabaf6a
kayecoul1 <kayecoul1@gmail.com>
2025-11-15 05:17:39 +0000

    modif

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
